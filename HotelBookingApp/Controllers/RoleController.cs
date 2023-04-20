02e04b384368d78341c4d770cdef17116c762ff7
mark <markc.balagot@gmail.com>
2023-04-21 03:51:10 +0800

    User controller and views

--- HotelBookingApp/Controllers/RoleController.cs
+++ HotelBookingApp/Controllers/RoleController.cs
@@ -1,12 +1,154 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelBookingApp.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelBookingApp.Controllers
 {
     public class RoleController : Controller
     {
-        public IActionResult Index()
+        public RoleManager<IdentityRole> _roleManager { get; }
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
         {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            //_roleManager.Roles;
+            //_roleManager.DeleteAsync();
+            //_roleManager.UpdateAsync();
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RoleViewModel roleViewModel, IdentityRole rolemodel)
+        {
+            if (!_roleManager.RoleExistsAsync(rolemodel.Name).GetAwaiter().GetResult())
+            {
+
+                if (ModelState.IsValid)
+                {
+                    //var newUser = new ApplicationUser
+                    //{
+                    //    Id = Guid.NewGuid(),
+                    //    UserName = "username",
+                    //    // Other user properties
+                    //};
+
+                    var role = new IdentityRole
+                    {
+                        Name = roleViewModel.Name
+                    };
+                    var result = await _roleManager.CreateAsync(role);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("GetAllRoles");
+                    }
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+                //popup message
+            }
+            //else
+            //popup message
+            // TempData["ErrorMessage"] = "User role already exist";
+            return View(roleViewModel);
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllRoles()
+        {
+            return View(_roleManager.Roles.ToList());
+            //RoleViewModel role = this._roleManager.to();
+            //return View(role);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(string? id)
+        {
+            //var role = await _roleManager.FindByIdAsync(id);
+            //var getroles = await _roleManager.GetRoleIdAsync(role);
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            var roleViewModel = new RoleViewModel
+            {
+                Name = role.Name
+            };
+
+            return View(roleViewModel);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(RoleViewModel roleViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(roleViewModel);
+            }
+
+            var existingRole = await _roleManager.FindByIdAsync(roleViewModel.Id.ToString());
+
+            if (existingRole == null)
+            {
+                return NotFound();
+            }
+
+            existingRole.Name = roleViewModel.Name;
+            existingRole.NormalizedName = roleViewModel.Name.ToUpperInvariant(); // Update the normalized name
+
+            var result = await _roleManager.UpdateAsync(existingRole);
+
+            if (!result.Succeeded)
+            {
+                // Handle the error
+            }
+
+            return RedirectToAction("GetAllRoles");
+        }
+
+        public async Task<IActionResult> Details(string? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // Find the role by ID
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+
+            // If role is not found, return NotFound result
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            // Convert IdentityRole to RoleViewModel
+            RoleViewModel roleViewModel = new RoleViewModel
+            {
+                Id = Guid.Parse(role.Id),
+                Name = role.Name
+            };
+
+            return View(roleViewModel);
+        }
+        public async Task<IActionResult> Delete(Guid Id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(Id.ToString());
+            var result = await _roleManager.DeleteAsync(role);
+            return RedirectToAction(controllerName: "Roles", actionName: "GetAllRoles");
         }
     }
 }
+
---
+++ HotelBookingApp/Controllers/UserController.cs
@@ -0,0 +1,174 @@
+﻿using HotelBookingApp.Data;
+using HotelBookingApp.Models;
+using HotelBookingApp.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        public RoleManager<IdentityRole> _roleManager;
+        HotelDbContext _db;
+
+        public UserController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, HotelDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = context;
+        }
+
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var userlist = await _userManager.Users.ToListAsync();
+            return View(userlist);
+        }
+
+        public async Task<IActionResult> Details(string? id)
+        {
+            ApplicationUser user = await this._userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            // Get the roles associated with the user
+            var userRole = await _userManager.GetRolesAsync(user);
+            IdentityRole role = await _roleManager.FindByNameAsync(userRole[0]);
+
+            ApplicationUser model = new ApplicationUser
+            {
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                PhoneNumber = user.PhoneNumber,
+                Role = role
+            };
+
+            return View(model);
+
+        }
+
+        public async Task<IActionResult> Delete(Guid Id)
+        {
+
+
+            ApplicationUser user = await _userManager.FindByIdAsync(Id.ToString());
+            var result = await _userManager.DeleteAsync(user);
+            // ApplicationUser user = await _userManager.FindByIdAsync(userId.ToString());
+            return RedirectToAction(controllerName: "Users", actionName: "GetAllUsers"); // reload the getall page it self
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            //var roleNames = _roleManager.Roles.Select(r => r.Name).ToList();
+            //ViewBag.Role = roleNames;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(RegisterViewModel userViewModel) // model binded this where the views data is accepted 
+        {
+            if (ModelState.IsValid)
+            {
+
+                var userModel = new ApplicationUser
+                {
+                    UserName = userViewModel.Email,
+                    Email = userViewModel.Email,
+                    FirstName = userViewModel.FirstName,
+                    LastName = userViewModel.LastName,
+                    PhoneNumber = userViewModel.PhoneNumber
+
+
+                };
+                var result = await _userManager.CreateAsync(userModel, userViewModel.Password);
+                if (result.Succeeded)
+                {
+                    var role = await _roleManager.FindByNameAsync("Guest");
+
+                    // Add the user to the role
+                    await _userManager.AddToRoleAsync(userModel, role.Name);
+                    return RedirectToAction("GetAllUsers");
+
+                    // notify user created POP_UP MESSAGE PLEASEE
+
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(userViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(ApplicationUser Userr, string id)
+        {
+
+            var roleManager = HttpContext.RequestServices.GetService(typeof(RoleManager<IdentityRole>)) as RoleManager<IdentityRole>;
+            var roles = await roleManager.Roles.ToListAsync();
+            ViewBag.listofrole = roles;
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            var usermodel = new ApplicationUser
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+
+            };
+
+
+            return View(usermodel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, ApplicationUser user)
+        {
+
+
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            ApplicationUser existingUser = await _userManager.FindByIdAsync(id);
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            // Update the user properties
+            existingUser.FirstName = user.FirstName;
+            existingUser.LastName = user.LastName;
+            existingUser.Email = user.Email;
+            existingUser.PhoneNumber = user.PhoneNumber;
+            existingUser.Role = user.Role;
+
+            // Update the user role
+            var userRole = await _userManager.GetRolesAsync(existingUser);
+            IdentityRole role = await _roleManager.FindByNameAsync(userRole[0]);
+            if (role == null)
+            {
+                return BadRequest();
+            }
+            await _userManager.RemoveFromRolesAsync(existingUser, userRole);
+            await _userManager.AddToRoleAsync(existingUser, role.Name);
+
+            // Update the user in the database
+            var result = await _userManager.UpdateAsync(existingUser);
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+            }
+
+            return RedirectToAction("GetAllUsers");
+        }
+    }
+}
